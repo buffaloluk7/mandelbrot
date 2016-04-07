911620cb619c00cd00a61fd96ea26c1cf23eb8fe
Thomas Eizinger <thomaseizinger@users.noreply.github.com>
2016-04-07 16:17:35 +0200

    Feature/continous zoom
    * reformat code
    introduce QUnit floatEqual extension
    add support for mouse wheel
    introduce new calculation method of next mandelbrot rectangle
    
    * introduce BigNumber.js to fix floating point precision errors
    
    * set fixed precision for websocket message
    
    * remove onclick handler

--- client/app/InputViewModel.js
+++ client/app/InputViewModel.js
@@ -1,47 +1,44 @@
-var InputViewModel = function(mandelbrotService, mandelbrotSpecCalculationService){
-    if (!(this instanceof InputViewModel)) return new InputViewModel(andelbrotService, mandelbrotSpecCalculationService);
+var InputViewModel = function (mandelbrotService, mandelbrotSpecCalculationService) {
+    if (!(this instanceof InputViewModel)) return new InputViewModel(mandelbrotService, mandelbrotSpecCalculationService);
 
-    var _getZoomInPercentage = function(){ return 90; }
-
-    var _getDefaultSpecs = function()
-    {
-        return{
-            width: _getMandelbrotView().offsetWidth,
-            height: _getMandelbrotView().offsetHeight,
+    var _getDefaultSpecs = function () {
+        return {
+            width: _mandelbrotView.offsetWidth,
+            height: _mandelbrotView.offsetHeight,
             iterations: 100,
-            minR: -3,
-            minI: -1.5,
-            maxR: 1,
-            maxI: 1.5
+            minR: new BigNumber(-3),
+            minI: new BigNumber(-1.5),
+            maxR: new BigNumber(1),
+            maxI: new BigNumber(1.5)
         }
-    }
-    var _currentSpecs;
-    var _getMandelbrotView = function(){ return document.getElementById("mandelbrotView");}
+    };
 
-    var _clickOnMandelbrot = function (event)
-    {
+    var _currentSpecs;
+    var _mandelbrotView = document.getElementById("mandelbrotView");
+
+    var _wheelOnMandelbrot = function (event) {
         var e = event.target;
         var dim = e.getBoundingClientRect();
         var x = event.clientX - dim.left;
         var y = event.clientY - dim.top;
 
+        var zoomIn = event.wheelDelta > 0;
+        _currentSpecs = mandelbrotSpecCalculationService.calculate(_currentSpecs, x, y, zoomIn ? 90 : 110);
 
-        _currentSpecs = mandelbrotSpecCalculationService.calculate(_currentSpecs, x, y, _getZoomInPercentage());
         mandelbrotService.getMandelbrot(_currentSpecs);
-
     };
 
-    var init = function(){
+    var init = function () {
 
         _currentSpecs = _getDefaultSpecs();
-        mandelbrotService.openWebSocket(_currentSpecs, function(data){
-            _getMandelbrotView().src = "data:image/jpg;base64," + data;
+        mandelbrotService.openWebSocket(_currentSpecs, function (data) {
+            _mandelbrotView.src = "data:image/jpg;base64," + data;
         });
 
-        _getMandelbrotView().onclick = _clickOnMandelbrot;
+        _mandelbrotView.onwheel = _wheelOnMandelbrot;
     };
 
-    return{
+    return {
         init: init
     };
 };
